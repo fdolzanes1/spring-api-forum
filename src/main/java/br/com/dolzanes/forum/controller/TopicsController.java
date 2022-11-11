50f2ed751d049f27a941c12203b1c03dc808d3b8
FabianoDolzanes <fabiano.dolzanes@fit-tecnologia.org.br>
2022-11-11 19:39:45 -0400

    Configurando Spring Data JPA

--- src/main/java/br/com/dolzanes/forum/controller/TopicsController.java
+++ src/main/java/br/com/dolzanes/forum/controller/TopicsController.java
@@ -1,21 +1,30 @@
 package br.com.dolzanes.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.dolzanes.forum.dto.TopicoDTO;
-import br.com.dolzanes.forum.model.Curso;
 import br.com.dolzanes.forum.model.Topico;
+import br.com.dolzanes.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicsController {
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
 
 	@RequestMapping("/topics")
-	public List<TopicoDTO> listAll(){
-		Topico topico = new Topico("Duvidas", "Duvida com Spring", new Curso("Java", "Programação"));
-		return TopicoDTO.converter(Arrays.asList(topico));
+	public List<TopicoDTO> lista(String nomeCurso){
+		
+		if(nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDTO.converter(topicos);
+		} else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDTO.converter(topicos);
+		}		
 	}
 }
---
+++ src/main/java/br/com/dolzanes/forum/repository/TopicoRepository.java
@@ -0,0 +1,16 @@
+package br.com.dolzanes.forum.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.com.dolzanes.forum.model.Topico;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+	
+	List<Topico> findByCursoNome(String name);
+
+	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+    List<Topico> carregarPorNomeDoCurso(String nomeCurso);
+}
